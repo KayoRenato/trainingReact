3dd75165a6239b205720c2e938e157bfba34e262
Kayo.io <kayorenatocontato@gmail.com>
2022-12-29 15:00:50 -0300

    chore: add Props Only Read

---
+++ pages/basic/propsOnlyRead.jsx
@@ -0,0 +1,6 @@
+import SumOne from '../../components/SumOne'
+
+
+export default function propsOnlyRead() {
+    return <SumOne number={99} />
+}
--- pages/css/integration2.jsx
+++ pages/css/integration2.jsx
@@ -2,7 +2,7 @@ import style from './integration2.module.css'
 
 export default function integration(){
     return (
-        <div >
+        <div id={style.integration2} className={style.integration2} >
             <div className={style.red}> Text #01 - Module</div>
             <div className={style['blue']}> Text #02 - Module</div>
             <div className={style.white}> Text #03 - Module</div>
